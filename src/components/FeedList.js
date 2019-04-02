7b81236b9f9138b23bd6916fb8d0ddcb6855e344
Caio Jhonny <caiojhonnyp@gmail.com>
2019-04-02 19:32:10 -0300

    Add nav category

--- src/components/FeedList.js
+++ src/components/FeedList.js
@@ -10,14 +10,23 @@ class FeedList extends React.Component {
     return (
       <Page>
         <section className="feed-list">
-          <React.Fragment>
-            <Dog />
-            <Dog />
-            <Dog />
-            <Dog />
-            <Dog />
-            <Dog />
-          </React.Fragment>
+            <nav className="nav">
+                <button className="nav-item">Husky</button>
+                <span className="nav-separator">/</span>
+                <button className="nav-item">Hound</button>
+                <span className="nav-separator">/</span>
+                <button className="nav-item">Pug</button>
+                <span className="nav-separator">/</span>
+                <button className="nav-item">Labrador</button>
+            </nav>
+            <React.Fragment>
+                <Dog />
+                <Dog />
+                <Dog />
+                <Dog />
+                <Dog />
+                <Dog />
+            </React.Fragment>
         </section>
       </Page>
     )
