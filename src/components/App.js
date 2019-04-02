2b23abb97d84232db581d1416d5dd284c4bfed87
Caio Jhonny <caiojhonnyp@gmail.com>
2019-04-02 19:09:00 -0300

    Add base folders

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './styles/App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <h1 className="title-app">
+        The <strong>IDDOG</strong>
+      </h1>
+    );
+  }
+}
+
+export default App;
