c576e1b6715860ca837b445c7c0ce6e18f58508d
Caio Jhonny <caiojhonnyp@gmail.com>
2019-04-02 19:21:13 -0300

    Add Page component

---
+++ src/components/Page.js
@@ -0,0 +1,23 @@
+import React, { Fragment } from 'react'
+import styled from 'styled-components';
+import './styles/Page.scss';
+
+const Wrapper = styled.div`
+  height: '100%',
+  flex: 1,
+  padding: '0 1rem;'
+`
+
+export default function Page({ children }) {
+
+  return (
+    <Fragment>
+      <div className="app">
+        <h1 className="title-app">
+          The <strong>IDDOG</strong>
+        </h1>
+        <Wrapper>{children}</Wrapper>
+      </div>
+    </Fragment>
+  )
+}
